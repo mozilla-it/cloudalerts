9c320b6fc76488ba0468127c899091a285d76444
Stewart Henderson <shenderson@mozilla.com>
2020-07-09 19:31:32 -0500

    Init loading correction

--- tests/unit/cloudalerts/v1/loggers/test_init.py
+++ tests/unit/cloudalerts/v1/loggers/test_init.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from .alert_logging import AlertLogger
-from .cloud_logging import CloudLogging
 
-__all__ = ["AlertLogger", "CloudLogging"]
+def test__init__():
+    from cloudalerts.v1.loggers import __all__
+
+    expected = ["AlertLogger", "CloudLogging"]
+    assert expected == __all__
